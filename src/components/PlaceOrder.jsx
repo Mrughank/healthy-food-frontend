2aaed032e4bf065502d1dfd99cf5ba562e855c98
Mrughank <mrugankchaudhary@gmail.com>
2025-12-10 12:08:00 +0530

    Fix cart mobile css & update route

--- src/components/PlaceOrder.jsx
+++ src/components/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useCart } from "./CartContext";
 import axios from "axios";
 import { useAuth } from "./AuthContext";
+import './PlaceOrder.css'
 
 export default function PlaceOrder() {
   const { cart } = useCart();
